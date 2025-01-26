4934b4976e4d3c18cbce6244ab0ee35bf6d6a6a6
Oleksandr Polieno <polyenoom@gmail.com>
2025-01-26 18:24:49 +0700

    confirm product

--- web/src/views/ProductView.vue
+++ web/src/views/ProductView.vue
@@ -45,6 +45,22 @@
           </tbody>
         </table>
       </div>
+
+      <div
+        v-if="
+          details &&
+          !(details.confirmed_by || details.rejected_by) &&
+          isRetailer
+        "
+        class="flex flex-row space-x-2"
+      >
+        <button class="btn btn-primary" v-on:click="confirmProduct">
+          Confirm
+        </button>
+        <button class="btn btn-secondary" v-on:click="rejectProduct">
+          Reject
+        </button>
+      </div>
     </article>
   </Navbar>
 </template>
@@ -54,20 +70,47 @@ import { onMounted, ref } from "vue";
 import { httpClient } from "@/services/http.js";
 import router from "@/router/index.js";
 import Navbar from "@/components/Navbar.vue";
+import { useMeStore } from "@/stores/index.js";
+import { storeToRefs } from "pinia";
+
+const meStore = useMeStore();
+
+const { isRetailer } = storeToRefs(meStore);
 
 const productId = ref(parseInt(router.currentRoute.value.params.id));
 const details = ref(null);
 const loading = ref(true);
 
-onMounted(async () => {
+const confirmProduct = async () => {
+  try {
+    await httpClient.post(`/api/products/${productId.value}/confirm`);
+    await loadProduct();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const rejectProduct = async () => {
+  try {
+    await httpClient.post(`/api/products/${productId.value}/reject`);
+    await loadProduct();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const loadProduct = async () => {
   loading.value = true;
   try {
-    const res = await httpClient.get(`/api/products/${productId.value}`);
-    details.value = res;
+    details.value = await httpClient.get(`/api/products/${productId.value}`);
   } catch (error) {
     console.error(error);
   } finally {
     loading.value = false;
   }
+};
+
+onMounted(async () => {
+  await loadProduct();
 });
 </script>
