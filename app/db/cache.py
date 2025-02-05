e2078ede69c0b6d46374bfaaf7c9ee156df4b0e7
Oleksandr Polieno <polyenoom@gmail.com>
2025-02-05 18:10:01 +0700

    cache table

---
+++ app/db/cache.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from decimal import Decimal
+
+import sqlalchemy as sa
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql import func
+
+from .base import Base
+
+
+class CacheTable(Base):
+    __tablename__ = "cache"
+
+    id: Mapped[int]
+    supplier_id: Mapped[int] = mapped_column(sa.ForeignKey("suppliers.id"))
+    retailer_id: Mapped[int] = mapped_column(sa.ForeignKey("retailers.id"), index=True)
+
+    cache_products: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    cache_payments: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    cache_due_date: Mapped[datetime] = mapped_column(nullable=True)
+    cache_to_pay: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    rtgs_products: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    rtgs_payments: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    rtgs_due_date: Mapped[datetime] = mapped_column(nullable=True)
+    rtgs_to_pay: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    fine_products: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    fine_payments: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+    fine_due_date: Mapped[datetime] = mapped_column(nullable=True)
+    fine_to_pay: Mapped[Decimal] = mapped_column(type_=sa.DECIMAL)
+
+    version: Mapped[int] = mapped_column(server_default="1")
+
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        onupdate=func.now(),
+    )
+
+    __table_args__ = (
+        sa.PrimaryKeyConstraint("id"),
+        sa.Index(
+            "idx_cache_supplier_retailer",
+            "supplier_id",
+            "retailer_id",
+        ),
+    )
